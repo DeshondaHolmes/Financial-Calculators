fc0b669c3726b64633f29c02ee6925896ec9c76d
Deshonda Holmes <Deshonda.holmes@gmail.com>
2023-05-01 00:03:01 -0400

    Add index.html and calculate pages with scripts

---
+++ scripts/future.cd.js
@@ -0,0 +1,14 @@
+"Use strict";
+
+/* 
+Calculate CD Formula
+
+A=P(1+r/n)**(nt)
+
+A=total CD will be wort at the end of the term , including amount input
+P= principal , or amount inout when you bought CD 
+R= Rate or APY (Annual interest rate, expressed as decimal)EX : interest rate 1.25 APY , r is 0.0125
+n= number of times that interest in compaunded every year , (n=365,interest compounded daily )
+t=time , number of years until maturity date
+
+*/
---
+++ scripts/mortgage.js
@@ -0,0 +1,14 @@
+"Use strict";
+
+/* 
+Calculate payment for loan 
+
+
+M  =  P * i**monthly/1-(1+i**monthly)**-L`months
+
+
+M=Monthly principal and interest paymeny
+P=Principal (loan amount)
+i=monthly interest rate , decimal form (ex 1% =0.01)
+L=length of loan in monhts
+*/
